7832ed5541ee4267d347b8fd7499b0d4e39a1b0a
Lambodar Thakur <lambodart@snapwork.com>
2024-09-04 17:33:59 +0530

    db error

--- src/main/java/com/security/model/MyUserRepository.java
+++ src/main/java/com/security/model/MyUserRepository.java
@@ -4,7 +4,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MyUserRepository extends JpaRepository<MyUser, Long> { 
+public interface MyUserRepository extends JpaRepository<MyUser, String> { 
 
     Optional<MyUser> findByUsername(String username);
 }
